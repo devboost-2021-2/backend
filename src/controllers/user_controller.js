fb27f85b7c9c206064ccc291e4852de1c7be9365
mariliatd <mariliatd@gmail.com>
2021-11-14 19:45:11 -0300

    Add user login
    Signed-off-by: mariliatd <mariliatd@gmail.com>
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
    Co-authored-by: Larissa Medeiros <larissavitoriamedeiros@usp.br>
    Co-authored-by: Pedro Mariano <ph_marianocorrea@hotmail.com>
    Co-authored-by: Willian Mori <wnmrmr@gmail.com>
    Co-authored-by: João Gabriel Josephik <joao.gabrielaaj@gmail.com>

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,38 +1,51 @@
 class UserController {
   static list = [
     {
-      username: "devboost",
-      password: "senhaforte",
-      realname: "João",
-      email: "joaozinho@gmail.com",
-      course: "BCC"
-    }
+      username: 'devboost',
+      password: 'senhaforte',
+      realname: 'João',
+      email: 'joaozinho@gmail.com',
+      course: 'BCC',
+    },
   ];
   static create(req, res) {
-    const {
-      username,
-      password,
-      realname,
-      email,
-      course
-    } = req.body
-    const has_user = UserController.list.find((user) => 
-      user.username === username || user.email === email
+    const { username, password, realname, email, course } = req.body;
+    const has_user = UserController.list.find(
+      (user) => user.username === username || user.email === email
     );
-    if( has_user ) {
-      return res.status(400).json({ message: has_user.username === username ?
-	"Usuário já existente" :
-	"Email já existente"
-      });
+    if (has_user) {
+      return res
+        .status(400)
+        .json({
+          message:
+            has_user.username === username
+              ? 'Usuário já existente'
+              : 'Email já existente',
+        });
     }
     UserController.list.push({
       username,
       password,
       realname,
       email,
-      course
+      course,
     });
-    return res.status(201).json({ message: "Usuário criado com sucesso" })
+    return res.status(201).json({ message: 'Usuário criado com sucesso' });
+  }
+
+  static login(req, res) {
+    const { user, password } = req.body;
+    const has_user = UserController.list.find(
+      (item) =>
+        (item.username === user || item.email === user) &&
+        item.password == password
+    );
+    if (!has_user) {
+      return res.status(404).json({ message: 'Usuário e/ou senha errados' });
+    }
+    const response = Object.assign({}, has_user);
+    delete response.password;
+    return res.status(200).json(response);
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ import UserController from './controllers/user_controller.js';
 const router = Router();
 
 router.post('/users', UserController.create);
+router.post('/users/login', UserController.login);
 
 export default router;
