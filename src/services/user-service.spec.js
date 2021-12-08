ce65b596d6bd7f80baae9343cae741a1b2110589
mariliatd <mariliatd@gmail.com>
2021-12-08 20:24:45 -0300

    Finish user creation tests
    Signed-off-by: mariliatd <mariliatd@gmail.com>
    Co-authored-by: Pedro Mariano <ph_marianocorrea@hotmail.com>
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
    Co-authored-by: Vinicius Frota <viniciuspxfrota@gmail.com>

--- src/services/user-service.spec.js
+++ src/services/user-service.spec.js
@@ -18,7 +18,7 @@ describe('User create', () => {
   });
 
   describe('When user creation fails', () => {
-    it('throws an error for user with same username', () => {
+    it('throws an error for user with same username and email', () => {
       const user = {
         username: 'devboost',
         password: 'senhaforte',
@@ -30,6 +30,34 @@ describe('User create', () => {
       expect(() => create(user, list)).toThrowError(
         'Usuário e email já existentes'
       );
+    });
+
+    it('throws an error for user with same username', () => {
+      const user = {
+        username: 'devboost',
+        password: 'senhaforte',
+        realname: 'João',
+        email: 'joaozinho@gmail.com',
+        course: 'BCC',
+      };
+      const list = [{ ...user }];
+      expect(() =>
+        create({ ...user, email: 'devboost@gmail.com' }, list)
+      ).toThrowError('Usuário já existente');
+    });
+
+    it('throws an error for user with same email', () => {
+      const user = {
+        username: 'devboost',
+        password: 'senhaforte',
+        realname: 'João',
+        email: 'joaozinho@gmail.com',
+        course: 'BCC',
+      };
+      const list = [{ ...user }];
+      expect(() =>
+        create({ ...user, username: 'joaozinho' }, list)
+      ).toThrowError('Email já existente');
     });
   });
 });
